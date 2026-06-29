f7dcfca1181767b7864aa467b1e5445391058477
Ethan Herman <ethan@Mac.localdomain>
2026-06-28 20:57:36 -0700

    Initialize red wgfx window

--- src/main.cpp
+++ src/main.cpp
@@ -10,9 +10,38 @@ int main() {
         return 1;
     }
 
-    wgfx::init();
-    std::cout << "SDL3 and wgfx initialized\n";
+    SDL_Window* window = SDL_CreateWindow("wgfx red window", 800, 600, SDL_WINDOW_RESIZABLE);
+    if (!window) {
+        std::cerr << "SDL window creation failed\n";
+        SDL_Quit();
+        return 1;
+    }
 
+    wgpu::Surface surface = wgfx::getSurface(window);
+    wgfx::init(surface);
+
+    wgfx::ColorTexture backbuffer;
+    wgfx::RenderPass redPass;
+    redPass.addTarget(&backbuffer);
+    redPass.setClear({ 1.0, 0.0, 0.0, 1.0 });
+
+    bool running = true;
+    while (running) {
+        SDL_Event event;
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_EVENT_QUIT) {
+                running = false;
+            }
+        }
+
+        wgfx::start();
+        wgfx::touch(&backbuffer);
+        redPass.prepare();
+        redPass.end();
+        wgfx::frame();
+    }
+
+    SDL_DestroyWindow(window);
     SDL_Quit();
     return 0;
 }
